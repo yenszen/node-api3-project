7e3cd270e7ab76d95042e73212e754f6ec12881b
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-14 22:23:36 +0800

    completed logger custom middleware

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(logger);
+
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl} ${new Date()}`);
+  next();
+}
 
 module.exports = server;
